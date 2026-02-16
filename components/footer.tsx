e6864807a8325487cdd3a85586fa3fe6273dfe9d
Jinsu Hong <kaameo12@gmail.com>
2026-02-16 21:59:29 +0900

    chore: 푸터 저작권 연도 2026으로 업데이트

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
       <div className="px-4 md:px-6 lg:px-8 max-w-[1400px] mx-auto flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            © 2025 Kaameo. Built with{' '}
+            © 2026 Kaameo. Built with{' '}
             <Link
               href="https://nextjs.org"
               target="_blank"
